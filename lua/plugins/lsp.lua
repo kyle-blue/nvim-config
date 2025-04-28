85dc925454cf6ab379f66671d701c4bd3f488b44
Kyle Doidge <kyle.blue.doidge@gmail.com>
2025-04-28 14:35:01 +0100

    Add basic pylsp configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
--- Includes multiple lsp related plugins and configuration
+-- Includes multiple lsp related plugins and configurationlsp
 
 return {
     {
@@ -151,34 +151,32 @@ return {
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
-                -- gopls = {},
-                -- pyright = {},
+                pylsp = {
+                    settings = {
+                        pylsp = {
+                            plugins = {
+                                autopep8 = { enabled = false },
+                                mccabe = { enabled = false },
+                                preload = { enabled = false },
+                                pycodestyle = { enabled = false },
+                                pyflakes = { enabled = false },
+                                pylint = { enabled = true },
+                                yapf = { enabled = false },
+                            },
+                        },
+                    },
+                },
                 rust_analyzer = {},
-                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                --
-                -- Some languages (like typescript) have entire language plugins that can be useful:
-                --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`ts_ls`) will work just fine
                 ts_ls = {},
-                --
-
                 lua_ls = {
-                    -- cmd = { ... },
-                    -- filetypes = { ... },
-                    -- capabilities = {},
                     settings = {
                         Lua = {
                             completion = {
                                 callSnippet = 'Replace',
                             },
-                            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                            -- diagnostics = { disable = { 'missing-fields' } },
                         },
                     },
                 },
-
                 jsonls = {
                     filetypes = { 'json', 'jsonc' },
                     settings = {
