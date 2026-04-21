052d84665229c6e7a64eb95a0fda8148b06e56d9
Kyle Doidge <kyle.blue.doidge@gmail.com>
2026-04-21 12:16:06 +0100

    Add persistance / auto-session plugin

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,5 +1,13 @@
 return {
-	"folke/persistence.nvim",
-	event = "BufReadPre",
-	opts = {},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/" },
+		},
+	},
 }
